32b5af3dba92bd5f07e8db84f0bc83750eee8755
Ravi63444 <53789453+Ravi63444@users.noreply.github.com>
2019-08-26 20:56:24 +0530

    Robofriends push

---
+++ build/precache-manifest.81904a524b1dae8eeabbf038cfc1a832.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5551e2b41885e52c0fd61195772242b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13464a649aa1c96fd8b0",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "0c36c7b0ab4b0a9f38be",
+    "url": "/static/css/main.9d4fa375.chunk.css"
+  },
+  {
+    "revision": "13464a649aa1c96fd8b0",
+    "url": "/static/js/2.541164eb.chunk.js"
+  },
+  {
+    "revision": "0c36c7b0ab4b0a9f38be",
+    "url": "/static/js/main.8c3be11f.chunk.js"
+  },
+  {
+    "revision": "b0a546bcc125fb21d6be",
+    "url": "/static/js/runtime~main.75271b63.js"
+  }
+]);
